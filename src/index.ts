2716373901619bbf66aa3595f112fc3483cf54ae
naka <nakasyo2792@gmail.com>
2022-09-12 10:13:36 +0900

    webpack.config

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+
+
+import SaySomething from "./saySomething"
+
+//htmlElement型のroot変数を用意してるだけ
+const root: HTMLElement | null = document.getElementById("root")
+
+//インスタンス化
+const saySomething = new SaySomething("Hello Typescript")
+saySomething.sayText(root)
---
+++ src/saySomething.ts
@@ -0,0 +1,18 @@
+
+
+export default class SaySomething {
+    //クラスで使うプロパティ
+    message: string;
+
+    //初期化
+    constructor(message: string) {
+        this.message = message
+    }
+
+    //外部から呼び出せるメソッドを定義
+    public sayText(elem: HTMLElement | null) {
+        if(elem) {
+            elem.innerText = this.message
+        }
+    }
+}
